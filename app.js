91db376e6deb3381f2fd35656dddc65c466b93f7
reizhiger <reimon1124@gmail.com>
2019-11-04 11:17:18 -0500

    setting view engine to ejs and creating views folder for user views

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
  
+//setting view engine
+app.set('view engine', 'ejs');
+
+/*
+    ROUTES
+*/
+
+// GET /
 app.get('/', function (req, res) {
-  res.send('Hello Rei')
-})
+  res.render('home.ejs');
+});
  
 app.listen(3000, function(){
     console.log('server is live on port: 3000');
